55cfc4545590aebd0ad4cf040ac23d0b1b51215d
JOSE MARCOS <joseluismarcosramos@outlook.com>
2026-06-19 19:08:35 -0500

    correcion

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client'],
   // RUC como ruta base: https://dominio.com/10721468688
-  basePath: '/10721468688',
 }
 
 export default nextConfig
